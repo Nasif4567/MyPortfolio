45626ed7f44833961990bb4ddc0be2edee29f8ce
Nasif4567 <119555278+Nasif4567@users.noreply.github.com>
2024-06-01 09:04:34 +0100

    Add files via upload

---
+++ js/githubapi.js
@@ -0,0 +1,59 @@
+async function listRepoIssues() {
+  try {
+    const loadingSpinner = document.getElementById('loadingSpinner');
+    loadingSpinner.style.display = 'hide'; // Show loading spinner
+    
+    const response = await fetch('https://api.github.com/user/repos', {
+      headers: {
+        'Authorization': 'Bearer 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',
+        'Accept': 'application/vnd.github.v3+json',
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch repositories');
+    }
+
+    const data = await response.json();
+    
+    const repoContainer = document.getElementById('repoContainer');
+    const assetsFolder = 'shuffle/';
+    const images = ['Picture1.jpg', 'Picture2.jpg', 'Picture3.jpg', 'Picture4.jpg'];
+    shuffleArray(images); // Shuffle the images array
+
+    for (let i = 0; i < data.length; i++) {
+      const card = document.createElement('div');
+      card.classList.add('card');
+
+
+    loadingSpinner.style.display = 'none';
+
+
+      const repoIndex = i % data.length; // Use modular arithmetic to repeat repo data
+      const imageIndex = i % images.length; // Use modular arithmetic to repeat images
+      const cardContent = `
+        <img src="${assetsFolder}${images[imageIndex]}" alt="Image ${imageIndex + 1}">
+        <h3>${data[repoIndex].name}</h3>
+        <p>${data[repoIndex].description}</p>
+        <a href="${data[repoIndex].html_url}" target="_blank">View on GitHub</a>
+      `;
+      card.innerHTML = cardContent;
+
+      repoContainer.appendChild(card);
+    }
+
+    console.log("Response data:", data);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+listRepoIssues();
---
+++ js/scripts.js
@@ -0,0 +1,80 @@
+// Slide to Different Pages page 
+function slideTo(page, event) {
+    event.preventDefault();
+  
+    document.body.style.transition = 'transform 1s ease-in-out';
+    document.body.style.transform = 'translateX(-100%)';
+  
+    setTimeout(() => {
+      window.location.href = page;
+    }, 1000);
+}
+
+//
+
+//--------------------------------------Download cv ----------------------------------------
+
+  function downloadCV() {
+    const fileUrl = 'CV/cv.pdf';
+    const link = document.createElement('a');
+    link.href = fileUrl;
+    link.download = 'Your_CV_Name.pdf'; 
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+
+
+//----------------------------------------------------------------
+// Header Reusable 
+let headerElement = document.createElement('header');
+headerElement.innerHTML = `
+  <div class="navbar">
+    <a class="navlink" href="#" onclick="slideTo('index.html', event)">Home</a>
+    <a class="navlink" href="#" onclick="slideTo('About.html', event)">About</a>
+    <a class="navlink" href="#" onclick="slideTo('Works.html', event)">Work</a>
+    <p onclick="slideTo('index.html', event)">Ahmed's Portfolio</p>
+    <button onclick="slideTo('Contact.html', event)">Let's Talk</button>
+    <i class="fa fa-bars" onclick="toggleActive()"></i>
+    
+  </div>
+`;
+// Append the header to the body of the document
+document.body.appendChild(headerElement);
+//------------------------------------------------------------
+
+
+class Footer extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = `
+        <div class="FooterSocials">
+          <a href="https://www.facebook.com/ahmed.nasif.shahriar/" target="_blank"><i class="fab fa-facebook"></i></a>
+          <a href="https://twitter.com/?lang=en-gb" target="_blank"><i class="fab fa-twitter"></i></a>
+          <a href="https://www.linkedin.com/in/ahmed-nasif-8015b6124/" target="_blank"><i class="fab fa-linkedin"></i></a>  
+        </div>
+
+        <div class="FooterBotom">
+          <a  href="index.html">Home</a>
+          <a href="About.html">About</a>
+          <a href="Works.html">Work</a>
+        </div>
+
+        <div class="FooterInfo">
+          <p class="copyright">© 2024 Ahmed's Portfolio</p>
+          <p class="imprint">IMPRINT</p>
+        </div>
+      `;
+    }
+  }
+
+  customElements.define('footer-custom', Footer);
+
+
+  function toggleActive() {
+    const activeDiv = document.getElementById("activeDiv");
+    if (activeDiv.style.display === "none") {
+      activeDiv.style.display = "block";
+    } else {
+      activeDiv.style.display = "none";
+    }
+  }
